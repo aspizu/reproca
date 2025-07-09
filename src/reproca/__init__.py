48949b8658e25726da9df46f6f4e38b79f41c4d0
aspizu <aspizu@protonmail.com>
2025-07-09 16:38:45 +0530

    feat: rework implementation

--- src/reproca/__init__.py
+++ src/reproca/__init__.py
@@ -1,104 +1,219 @@
-# ruff: noqa: PLW2901
-
 from __future__ import annotations
 
+from errno import EFAULT
 from http import HTTPStatus
 from inspect import signature
-from types import UnionType
-from typing import TYPE_CHECKING, Any, final, get_origin, get_type_hints
+from typing import TYPE_CHECKING, Any, get_type_hints
 
 import msgspec
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
-from starlette.responses import JSONResponse, PlainTextResponse, Response
+from starlette.responses import PlainTextResponse, Response
 from starlette.routing import Route
+from typing_extensions import Sentinel
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Awaitable, Callable
 
     from starlette.requests import Request
 
-methods: list[type] = []
+_routes = []
 
+type Method = Callable[..., Awaitable[object]]
+
+
+def create_starlette_application(*args, **kwargs) -> Starlette:
+    return Starlette(*args, **kwargs, routes=_routes)
+
+
+def method(method: Method) -> Method:
+    parameters = get_parameters(method)
+
+    class Endpoint(HTTPEndpoint):
+        async def get(self, request: Request) -> Response:
+            return await get_method_help(method, parameters)
+
+        async def post(self, request: Request) -> Response:
+            return await invoke_method(method, request, parameters)
+
+    route = Route(f"/{method.__name__.replace('_', '-')}", Endpoint)
+    _routes.append(route)
+
+    return method
+
+
+def convert_case(text: str) -> str:
+    return text.lower().replace("-", "_")
+
+
+def collapse_case(_type: type, obj: Any) -> Any:
+    if isinstance(obj, dict):
+        return {convert_case(key): value for key, value in obj.items()}
+    return obj
+
+
+NODEFAULT = Sentinel("NODEFAULT")
 SPECIAL_PARAMETERS = ["return", "session", "request"]
 
 
-def jsoncast(text: str, expected: type) -> Any:
-    if expected is str:
-        return text
-    return msgspec.json.decode(text)
+class Parameter(msgspec.Struct):
+    name: str
+    type: object
+    default: object
 
 
-def method[**P, T](func: Callable[P, T]) -> Callable[P, T]:
+class Parameters(msgspec.Struct):
+    entries: dict[str, Parameter]
+    decoder: msgspec.json.Decoder[msgspec.Struct]
+    needs_request: bool
+
+
+def get_parameters(func: Callable[..., object]) -> Parameters:
+    entries = {}
     type_hints = get_type_hints(func)
-    sig = signature(func).parameters
-    parameter_fields = [
-        (key, type_hints[value.name])
-        if value.default is value.empty
-        else (key, type_hints[value.name], value.default)
-        for key, value in sig.items()
-        if key not in SPECIAL_PARAMETERS
-    ]
-    parameter_keys = {key.lower(): (key, type_) for key, type_, *_ in parameter_fields}
-    decoder = msgspec.json.Decoder(msgspec.defstruct(func.__name__, parameter_fields))
-    parameter_session_optional = False
-    if (obj := type_hints.get("session")) and get_origin(obj) is UnionType:
-        parameter_session_optional = True
+    for key, value in signature(func).parameters.items():
+        if key in SPECIAL_PARAMETERS:
+            continue
+        default = NODEFAULT if value.default is value.empty else value.default
+        entries[key.lower()] = Parameter(key, type_hints[key], default)
 
-    @final
-    class Method(HTTPEndpoint):
-        implementation = func
+    fields = []
+    for parameter in entries.values():
+        if parameter.default is NODEFAULT:
+            fields.append((parameter.name, parameter.type))
+        else:
+            fields.append((parameter.name, parameter.type, parameter.default))
+    decoder = msgspec.json.Decoder(msgspec.defstruct("X", fields))
 
-        async def get(self, request: Request) -> Response:
-            return PlainTextResponse(func.__doc__)
-
-        async def post(self, request: Request) -> Response:
-            body = {}
-            try:
-                body = msgspec.json.decode(
-                    await request.body(),
-                    type=dict[str, Any],
-                )
-            except msgspec.ValidationError as error:
-                return PlainTextResponse(str(error), status_code=HTTPStatus.BAD_REQUEST)
-            except msgspec.DecodeError:
-                pass
-            parameters = {}
-            for key, value in request.headers.items():
-                key = key.lower()
-                if key.startswith("x-"):
-                    key = key.removeprefix("x-")
-                    if key in parameter_keys:
-                        parameters[parameter_keys[key][0]] = jsoncast(
-                            value, parameter_keys[key][1]
-                        )
-            for key, value in request.query_params.items():
-                key = key.lower()
-                if key in parameter_keys:
-                    parameters[parameter_keys[key][0]] = jsoncast(
-                        value, parameter_keys[key][1]
-                    )
-            round_trip = msgspec.json.encode({**body, **parameters})
-            try:
-                obj = decoder.decode(round_trip)
-                kwargs = {key: getattr(obj, key) for key, _ in parameter_keys.values()}
-            except msgspec.ValidationError as error:
-                return PlainTextResponse(str(error), status_code=HTTPStatus.BAD_REQUEST)
-            if "request" in sig:
-                kwargs["request"] = request
-            response_data = await func(**kwargs)  # pyright: ignore[reportCallIssue, reportGeneralTypeIssues]
-            if isinstance(response_data, Response):
-                return response_data
-            return JSONResponse(response_data)
-
-    methods.append(Method)
-    return func
+    return Parameters(entries, decoder, needs_request="request" in type_hints)
 
 
-def create_starlette_application() -> Starlette:
-    return Starlette(
-        routes=[
-            Route(path=f"/{method.implementation.__name__}", endpoint=method)
-            for method in methods
-        ]
+def parse_parameter(text: str, type_: object) -> object:
+    if isinstance(type_, type) and issubclass(type_, str):
+        return text
+    return msgspec.json.decode(text, type=type_, dec_hook=collapse_case)
+
+
+async def parse_parameters(request: Request, parameters: Parameters) -> msgspec.Struct:
+    effective = {}
+
+    for key, value in request.query_params.items():
+        parameter = parameters.entries.get(convert_case(key))
+        if parameter is None:
+            continue
+        effective[parameter.name] = parse_parameter(value, parameter.type)
+
+    for key, value in request.headers.items():
+        key = key.lower()  # noqa: PLW2901
+        if not key.startswith("x-"):
+            continue
+        parameter = parameters.entries.get(convert_case(key.removeprefix("x-")))
+        if parameter is None:
+            continue
+        effective[parameter.name] = parse_parameter(value, parameter.type)
+
+    body = msgspec.json.decode(
+        await request.body() or b"{}", type=dict[str, Any], dec_hook=collapse_case
     )
+
+    round_trip = msgspec.json.encode({**effective, **body})
+    return parameters.decoder.decode(round_trip)
+
+
+async def invoke_method(
+    method: Method, request: Request, parameters: Parameters
+) -> Response:
+    try:
+        struct = await parse_parameters(request, parameters)
+    except msgspec.ValidationError as error:
+        return PlainTextResponse(str(error), HTTPStatus.BAD_REQUEST)
+    kwargs = {
+        parameter.name: getattr(struct, parameter.name)
+        for parameter in parameters.entries.values()
+    }
+    if parameters.needs_request:
+        kwargs["request"] = request
+    response = await method(**kwargs)
+    if isinstance(response, Response):
+        return response
+    return Response(msgspec.json.encode(response), media_type="application/json")
+
+
+def _format_method_info(method: Method) -> str:
+    method_name = method.__name__.replace("_", "-")
+    text = f"Method: {method_name}\n"
+    text += f"Endpoint: /{method_name}\n\n"
+
+    if method.__doc__:
+        text += f"Description:\n{method.__doc__.strip()}\n\n"
+
+    return text
+
+
+def _format_parameters(parameters: Parameters) -> str:
+    if not parameters.entries:
+        return ""
+
+    text = "Parameters:\n"
+    for param in parameters.entries.values():
+        param_type = getattr(param.type, "__name__", str(param.type))
+        if param.default is not NODEFAULT:
+            text += (
+                f"  {param.name} ({param_type}, optional, default: {param.default})\n"
+            )
+        else:
+            text += f"  {param.name} ({param_type}, required)\n"
+    return text + "\n"
+
+
+def _get_example_value(param: Parameter) -> str:
+    if param.default is not NODEFAULT:
+        return (
+            f'"{param.default}"'
+            if isinstance(param.default, str)
+            else str(param.default)
+        )
+
+    param_type = getattr(param.type, "__name__", str(param.type))
+    type_examples = {
+        "str": '"example"',
+        "string": '"example"',
+        "int": "42",
+        "integer": "42",
+        "float": "3.14",
+        "number": "3.14",
+        "bool": "true",
+        "boolean": "true",
+    }
+    return type_examples.get(param_type, '"value"')
+
+
+def _format_usage_example(method: Method, parameters: Parameters) -> str:
+    method_name = method.__name__.replace("_", "-")
+    text = "Usage:\n"
+    text += f"POST /{method_name}\n"
+    text += "Content-Type: application/json\n\n"
+
+    if parameters.entries:
+        text += "Example request body:\n"
+        text += "{\n"
+        param_items = list(parameters.entries.values())
+        for i, param in enumerate(param_items):
+            comma = "," if i < len(param_items) - 1 else ""
+            example_value = _get_example_value(param)
+            text += f'  "{param.name}": {example_value}{comma}\n'
+        text += "}\n\n"
+
+    text += "Parameters can also be passed via:\n"
+    text += "- Query parameters: ?param=value\n"
+    text += "- Headers: X-Param: value\n"
+
+    return text
+
+
+async def get_method_help(method: Method, parameters: Parameters) -> Response:
+    text = _format_method_info(method)
+    text += _format_parameters(parameters)
+    text += _format_usage_example(method, parameters)
+
+    return PlainTextResponse(text)
